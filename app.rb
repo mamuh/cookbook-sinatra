38e98f271cadb2ebb25be71f27931b6c7ace1e81
Mariana Caramori <musardo@mail.com>
2020-05-04 20:53:23 -0300

    create new recipe working

--- app.rb
+++ app.rb
@@ -3,16 +3,30 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+require_relative 'cookbook'
+require_relative 'recipe'
+
+csv_file   = File.join(__dir__, 'recipes.csv')
+cookbook   = Cookbook.new(csv_file)
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  @recipes = @cookbook.all
+  @recipes = cookbook.all
   erb :index
 end
 
-get '/about' do
-  erb :about
+get '/new' do
+  erb :new
+end
+
+post '/create' do
+  new_recipe = Recipe.new(params[:name], params[:description])
+  cookbook.add_recipe(new_recipe)
+  @recipes = cookbook.all
+  erb :index
 end
